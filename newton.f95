0d0c56f3b734b6417a27acdde31d0eaf2f2194c3
Rafael Goto <rafael916goto@hotmail.com>
2018-04-17 11:46:35 -0300

    Update newton.f95

--- newton.f95
+++ newton.f95
@@ -1,33 +1,20 @@
 PROGRAM newton
-  CHARACTER(len=32) :: arg
-  REAL :: x, aux
-  INTEGER :: i
-  i = 1
+    Implicit None
+
+    CHARACTER(len=32) :: arg
+    REAL*8 :: x, aux, result
+    INTEGER :: i
+    i = 1
     CALL getarg(i, arg) !conseguindo os arqumentos de ARG
-    READ (arg, *) x     !transformando string para REAL
+    READ (arg, *) x    !transformando string para REAL
 
     aux = x
     x = x/2
 
-    do 20 i = 1, 10, 1
-         x = raiz(x, aux)
-         WRITE (*,*) x
+    do 20 i = 1, 15, 1
+        result = (x - (((x*x)-aux) / (2*x)))
+        x = result
     20 continue
 
-
+    PRINT *, result
 END PROGRAM newton
-
-FUNCTION fx(x, y) RESULT(num)
-REAL :: x, y, num
-    num = (x * x) - y
-END FUNCTION fx
-
-FUNCTION fxLinha(x) result(num)
-REAL :: x, num
-    num = 2 * x
-END FUNCTION fxLinha
-
-FUNCTION raiz(x, y) RESULT(num)
-REAL :: x, y, num
-    num = x - (fx(x, y) / fxLinha(x))
-END FUNCTION raiz
